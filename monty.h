82b09e53ccf026b7b4ac8b79309e2a837db370a1
Remenyr <puertaalejo02@gmail.com>
2023-01-04 12:43:51 -0500

    monty

--- monty.h
+++ monty.h
@@ -42,7 +42,7 @@ typedef struct instruction_s
 
 void pint(stack_t **sstack, unsigned int line);
 void pop(stack_t **stack, unsigned int line);
-void get_func(char *opcode, stack **cp_stack, unsigned int line);
+void get_func(char *opcode, stack_t **cp_stack, unsigned int line);
 void add(stack_t **stack, unsigned int line);
 void nop(stack_t **stack, unsigned int line);
 void swap(int *stack, int size);
